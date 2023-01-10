54fb0e62dc10011cf83105ba25efcb5cbc2a3caa
Elena <olena168@gmail.com>
2023-01-10 20:16:03 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [work, setWork] = useState(0);
+  const {id, image, name, executor, term} = data[work];
+  
+  const previousWork = () => {
+    setWork((work => {
+      work--;
+      if(work < 0) {
+        return data.length - 1;
+      }
+      return work;
+    }))
+  }
+
+  const nextWork = () => {
+    setWork ((work => {
+      work++;
+      if (work > data.length-1) {
+        work=0;
+      }
+      return work;
+    }))
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <img src={image} width='350px' alt='pic' />
+      </div>
+      <div className='container'>
+        <h1>Наименование работ: {id}. {name}</h1>
+      </div>
+      <div className='container'>
+        <h2>Исполнитель: {executor}</h2>
+      </div>
+      <div className='container'>
+        <h3>Срок выполнения: {term}</h3>
+      </div>
+      <div className='btn'>
+        <button onClick={previousWork}>previous</button>
+        <button onClick={nextWork}>next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const data = [
+    {
+        id: 1,
+        image: 'https://simplesail.com/images/1920w/1/assets/images/content/Articles/0028-1280.jpg',
+        name: 'Убрать люфт пера',
+        executor: 'Александр',
+        term: 'январь 2023'
+    },
+
+    {
+        id: 2,
+        image: 'https://destymarine.com/wp-content/uploads/2017/03/Yacht-Painting-Portsmouth.jpg',
+        name: 'Ремонт кормы', 
+        executor: 'Борис', 
+        term: 'январь 2023'
+    },
+
+    {
+        id: 3,
+        image: 'https://www.powerandmotoryacht.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1240/MTUwMjM2MTMyMjYzNjAyMTEx/quitena-rod-box2.webp',
+        name: 'Изготовление мебели', 
+        executor: 'Борис', 
+        term: 'февраль 2023'
+    },
+
+    {
+        id: 4,
+        image: 'https://ikyacht.com/wp-content/uploads/2017/07/YACHTREFITANDREPAIR-1.jpg',
+        name: 'Дизайн интерьера лодки', 
+        executor: 'Борис', 
+        term: 'февраль 2023'
+    },
+
+    {
+        id: 5,
+        image: 'https://lodki-lodki.ru/images/product_images/popup_images/2348_0.jpg',
+        name: 'Ремонт подвесного мотора', 
+        executor: 'Александр', 
+        term: 'январь 2023'
+    },
+
+    {
+        id: 6,
+        image: 'https://images.unsplash.com/photo-1542227500-8e8f1d03f876?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        name: 'Диагностика и ремонт двигателя', 
+        executor: 'Александр', 
+        term: 'январь 2023'
+    },
+
+    {
+        id: 7,
+        image: 'https://images.unsplash.com/photo-1561446121-011485c5cb73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+        name: 'Замена такелажа', 
+        executor: 'Денис', 
+        term: 'март 2023'
+    }
+]
